d3d08d3f9273d16a2e25357e93cd05b1e3188112
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2021-08-21 04:26:13 +0200

    first commit

---
+++ src/main/java/com/example/valueinsoftbackend/UserController.java
@@ -0,0 +1,18 @@
+package com.example.valueinsoftbackend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class UserController {
+
+    @GetMapping
+    public String getgreet()
+    {
+        return "hello";
+    }
+
+}
---
+++ src/main/java/com/example/valueinsoftbackend/ValueinsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.valueinsoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueinsoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueinsoftBackendApplication.class, args);
+	}
+
+}
