174485b53f130f491f8eb52c09d9dc0058895467
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2021-12-06 05:02:12 +0200

    qv5

--- src/main/java/com/example/valueinsoftbackend/Controller/AuthenticateController.java
+++ src/main/java/com/example/valueinsoftbackend/Controller/AuthenticateController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Locale;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", exposedHeaders="Access-Control-Allow-Origin")
 
 public class AuthenticateController {
     private AuthenticationRequest auth;
