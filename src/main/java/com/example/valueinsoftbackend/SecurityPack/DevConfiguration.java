e10056c6ad4586910dd00226de3ac8ccc06d8e8f
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2021-12-06 05:29:38 +0200

    qv5

---
+++ src/main/java/com/example/valueinsoftbackend/SecurityPack/DevConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Samir Filifl
+ */
+
+package com.example.valueinsoftbackend.SecurityPack;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+    }
+}
